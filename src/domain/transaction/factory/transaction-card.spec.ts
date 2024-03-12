c3f1b3121e30bd7f6b9d4c9600a02d31f781ade1
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2024-03-12 19:57:13 -0300

    feat: create unit test factory TransactionCard

---
+++ src/domain/transaction/factory/transaction-card.spec.ts
@@ -0,0 +1,11 @@
+import TransactionCard from "../entity/transaction-card";
+import TransactionCardFactory from "./transaction-card.factory";
+
+describe("Transaction factory unit tests", () => {
+  test("should create a transaction", () => {
+    const transaction = TransactionCardFactory.create("123", 100, "credit");
+    expect(transaction.idempotencyId).toBe("123");
+    expect(transaction.amount).toBe(100);
+    expect(transaction.type).toBe("credit");
+  });
+});
